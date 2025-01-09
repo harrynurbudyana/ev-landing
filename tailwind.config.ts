9b294a313b345e563ceea93560f33af15a4f6168
salmanida <salmanidauljannah@gmail.com>
2025-01-09 15:20:09 +0700

    update 5

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,15 +3,27 @@ import daisyui from "daisyui";
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+   "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/layouts/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/sections/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+       "olblue" : "#315f90",
+        "torque" : "#328D8F",
+        "tosca"  : "#328F62",
+        "ored"   : "#952323",
+        "lred"   : "#A73121",
+        "whSky"  : "#EEF5FF",
+        "whConfig" : "#F4F9FF"
       },
     },
   },
